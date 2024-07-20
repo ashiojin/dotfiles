86168ad07bb74dbeee74cd7014e77cec492580da
MORI Yuji <ashiojin@gmail.com>
2024-07-20 20:57:11 +0900

    Fix neovim install path & version

--- .bin/installer/install_neovim_latest.sh
+++ .bin/installer/install_neovim_latest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ue
 
-function install_neovim_latest() {
-    curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar zx --strip-components 1 -C ~/.bin/bin/
+function install_neovim_0_10() {
+    curl -L https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz | tar zx -C ~/.local.bin/
 }
 
-install_neovim_latest
+install_neovim_0_10
