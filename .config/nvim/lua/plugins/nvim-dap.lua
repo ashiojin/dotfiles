14b941f5e5b19374dc55c737b92e1201aacb3c0c
MORI Yuji <ashiojin@gmail.com>
2024-07-21 18:55:00 +0900

    Add nvim-dap-ui

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -31,9 +31,12 @@ return {
 
 
         -- Key Mappings
-        vim.keymap.set('n', '<f5>', function()
-            dap.continue()
-        end, { desc = 'Continue' })
+        vim.keymap.set('n', '<f5>', function() dap.continue() end, { desc = 'Continue' })
+        vim.keymap.set('n', '<f10>', function() dap.step_over() end, { desc = 'Step over' })
+        vim.keymap.set('n', '<f11>', function() dap.step_into() end, { desc = 'Step into' })
+        vim.keymap.set('n', '<f12>', function() dap.step_out() end, { desc = 'Step out' })
+        vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end, { desc = 'Toggle Breakpoint' })
+        vim.keymap.set('n', '<leader>dl', function() dap.run_last() end, { desc = 'Re-runs the last debug adapter/configuration' })
 
     end,
 }
