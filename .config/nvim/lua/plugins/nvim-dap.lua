1e04169a971ba746e0788c137d2f272d7335e194
MORI Yuji <ashiojin@gmail.com>
2024-07-22 21:57:42 +0900

    Add keymap for nvim-dap

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -36,6 +36,7 @@ return {
         vim.keymap.set('n', '<f11>', function() dap.step_into() end, { desc = 'Step into' })
         vim.keymap.set('n', '<f12>', function() dap.step_out() end, { desc = 'Step out' })
         vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end, { desc = 'Toggle Breakpoint' })
+        vim.keymap.set('n', '<leader>dt', function() dap.set_breakpoint(nil, nil, vim.fn.input('Trace point message: ')) end)
         vim.keymap.set('n', '<leader>dl', function() dap.run_last() end, { desc = 'Re-runs the last debug adapter/configuration' })
 
     end,
