1a532928f43d42e8ea93a2dcef18623b58349251
MORI Yuji <ashiojin@gmail.com>
2024-07-26 23:59:11 +0900

    <cr> on neo-tree window to open file & close window

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,8 @@ return {
     },
 
     opts = {
+        close_if_last_window = true,
+
         commands = {
             open_and_next = function(state)
                 local node = state.tree:get_node()
@@ -23,15 +25,32 @@ return {
                     state.commands['open'](state)
                 end
             end,
+            open_and_close_window = function(state)
+                local node = state.tree:get_node()
+                if node.type == 'directory' then
+                    -- open the direcotry if it closed,
+                    -- and then move to next (probably the first subnode)
+                    if not node:is_expanded() then
+                        state.commands['open'](state)
+                    end
+                    vim.api.nvim_feedkeys('j', 'n', false)
+                else
+                    state.commands['open'](state)
+                    state.commands['close_window'](state)
+                end
+            end
         },
         window = {
             mappings = {
-                ['<space><space>'] = {
+                ['<leader><space>'] = {
                     'toggle_node',
                     nowait = false,
                 },
                 ['l'] = {
                     command = 'open_and_next',
+                },
+                ['<cr>'] = {
+                    command = 'open_and_close_window',
                 },
                 ['h'] = {
                     'close_node',
@@ -46,13 +65,13 @@ return {
 
     init = function (lazy)
         vim.keymap.set(
-            'n', '<space>w', ':Neotree toggle reveal<CR>',
+            'n', '<space>w', ':Neotree action=focus toggle reveal source=filesystem<CR>',
             {
                 desc = ':Neotree'
             }
         )
         vim.keymap.set(
-            'n', '<space>b', ':Neotree toggle reveal buffers<CR>',
+            'n', '<space>b', ':Neotree action=focus toggle reveal source=buffers<CR>',
             {
                 desc = ':Neotree buffers'
             }
