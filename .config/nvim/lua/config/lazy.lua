ab68046ec43bca4d99dbcfaa28b5bfa802dd6b62
MORI Yuji <ashiojin@gmail.com>
2024-07-28 14:38:58 +0900

    Disable hererocks feature of lazy.nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -32,5 +32,8 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+
+  -- disable hererocks
+  rocks = { hererocks = false, },
 })
 
