5d9ce3e376b54a85e974fcadedd55654c322ab59
MORI Yuji <ashiojin@gmail.com>
2024-07-28 16:43:27 +0900

    Add trouble.nvim but disabled now

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,40 @@
+return {
+    "folke/trouble.nvim",
+    enabled = false, -- Use raw quickfix for now...
+    opts = {
+
+    },
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>xd",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xl",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xq",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
+}
