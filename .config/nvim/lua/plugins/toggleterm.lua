d44997b6ac51a9cf36c763a6fd6f0855a352b33d
MORI Yuji <ashiojin@gmail.com>
2024-08-14 11:41:15 +0900

    Add term mode keymap

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -6,4 +6,18 @@ return {
     version = '*',
     opts = {
     },
+    init = function()
+        local group = vim.api.nvim_create_augroup('ToggleTermKey', { clear = true })
+        vim.api.nvim_create_autocmd('TermOpen',
+            {
+                group = group,
+                callback = function()
+                    local opts = { buffer = 0 }
+
+                    -- window operations
+                    vim.keymap.set('t', '<c-w>', [[<c-\><c-n><c-w]], opts)
+                end
+            }
+        )
+    end
 }
