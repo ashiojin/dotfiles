936bc846c21409447209fb686ba0d9ffc47b134d
MORI Yuji <ashiojin@gmail.com>
2024-11-07 16:39:56 +0900

    Add workaround for -32802 err from rust-analyzer

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -7,7 +7,18 @@ return {
         opts = {
             server = {
                 on_attach = function(_, bufnr)
-                    -- TODO: Keymaps
+                    ----------------------------
+                    -- FIXME: it's WORKAROUND: https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+                    for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+                        local default_diagnostic_handler = vim.lsp.handlers[method]
+                        vim.lsp.handlers[method] = function(err, result, context, config)
+                            if err ~= nil and err.code == -32802 then
+                                return
+                            end
+                            return default_diagnostic_handler(err, result, context, config)
+                        end
+                    end
+                    ----------------------------
                 end,
                 default_settings = {
                     ['rust-analyzer'] = {
