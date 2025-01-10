bfa7d04a7734569aca71ff1e2182e114b2708f43
MORI, Yuji <ashiojin@gmail.com>
2025-01-10 22:27:19 +0900

    Fix install check for nvm

--- .bin/installer/install_nvm.sh
+++ .bin/installer/install_nvm.sh
@@ -2,7 +2,7 @@
 set -ue
 
 function install_nvm() {
-  if command -v nvm 2>&1 >/dev/null
+  if ! command -v nvm 2>&1 >/dev/null
   then
     PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash'
   fi
