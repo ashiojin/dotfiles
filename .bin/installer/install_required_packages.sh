c9bf55f452172cc8dcdcdc514a5faa3e20da0fa9
MORI, Yuji <ashiojin@gmail.com>
2025-01-13 11:58:09 +0900

    Add pkgconf

--- .bin/installer/install_required_packages.sh
+++ .bin/installer/install_required_packages.sh
@@ -4,6 +4,7 @@ set -ue
 function install() {
     sudo apt install -y curl wget gawk python3-pip unzip
     sudo apt install -y fd-find ripgrep
+    sudo apt install -y clang lld pkgconf
 
     # Ubuntu's fd package uses `fdfind` as the binary, not the expected `fd`.
     ln -snf $(which fdfind) ~/.local/bin/fd
