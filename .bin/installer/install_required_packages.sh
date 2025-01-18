42f3b83f26af45d5cff9ada049bd44d9e5814702
MORI, Yuji <ashiojin@gmail.com>
2025-01-18 13:15:04 +0900

    Add shellcheck

--- .bin/installer/install_required_packages.sh
+++ .bin/installer/install_required_packages.sh
@@ -4,8 +4,10 @@ set -ue
 function install() {
     sudo apt install -y curl wget gawk python3-pip unzip
     sudo apt install -y fd-find ripgrep
-    sudo apt install -y clang lld pkgconf
+    sudo apt install -y pkgconf libasound2-dev libudev-dev # required by rust-analyzer(to analyze bevy?)
 
+    sudo apt install -y clang lld
+    sudo apt install -y shellcheck
     # Ubuntu's fd package uses `fdfind` as the binary, not the expected `fd`.
     ln -snf $(which fdfind) ~/.local/bin/fd
 }
